65fd3f1b3e363e98303c2dc432e8f6ee3e89d874
Oday <oday@saltserv.com>
2025-11-17 15:44:10 +0100

    proposed fix to unauthorized issue

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -5,8 +5,9 @@ import useAuth from '../hooks/useAuth';
 const RequireAuth = ({allowedRoles}) => {
     const {auth} = useAuth();
     const location = useLocation();
-    const rolesArray = auth?.roles ? Object.values(auth.roles) : [];
-    const hasAllowedRole = allowedRoles ? rolesArray.some(role => allowedRoles.includes(role)) : true;
+
+    const hasAllowedRole = allowedRoles ? auth?.roles?.some(role => allowedRoles.includes(role)) : true;
+
     if (!auth?.accessToken) {
         return <Navigate to="/login" state={{from: location}} replace/>;
     } else if (!hasAllowedRole) {
